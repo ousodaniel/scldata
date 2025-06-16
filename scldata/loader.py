a765e55f10782fb1a1380ca72c06da20cfa2dee5
ousodaniel <ousodaniel@gmail.com>
2025-06-16 19:52:09 +0100

    train SCL2205 load

--- scldata/loader.py
+++ scldata/loader.py
@@ -7,5 +7,13 @@ from pathlib import Path
 def load(split: Union[str, int, None] = None) -> Union[pd.DataFrame, Tuple[pd.DataFrame]]:
     df_full = pd.read_csv('data/scl2205.csv', index_col='entry')
 
+    with open('data/splits.json', 'r') as f:
+        splits = json.load(f)
+
+    with open('data/entries.json', 'r') as f:
+        entries = json.load(f)
+
     if split is None or split == 'full':
         return df_full
+    elif split == 'train':
+        return df_full.loc[[entries[str(idx)] for idx in splits['trn']]]

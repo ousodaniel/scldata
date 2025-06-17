b8455773b3467df75c97be5a59b24568fa20cb35
ousodaniel <ousodaniel@gmail.com>
2025-06-17 15:27:22 +0100

    cache json files outside function

--- scldata/loader.py
+++ scldata/loader.py
@@ -2,6 +2,15 @@ import json
 import pandas as pd
 from typing import Tuple, Union
 
+with open('data/labels.json', 'r') as f:
+    labels = json.load(f)
+
+with open('data/splits.json', 'r') as f:
+    splits = json.load(f)
+
+with open('data/entries.json', 'r') as f:
+    entries = json.load(f)
+
 def load(split: Union[str, int, None] = None) -> Union[pd.DataFrame, Tuple[pd.DataFrame]]:
     """
     A function for loading the full or split SCL2205 dataset.
@@ -56,19 +65,9 @@ def load(split: Union[str, int, None] = None) -> Union[pd.DataFrame, Tuple[pd.Da
 
     df_full = pd.read_csv('data/scl2205.csv', index_col='entry')
 
-    with open('data/labels.json', 'r') as f:
-        labels = json.load(f)
-
     if split is None or split == 'full':
         return df_full.replace(labels)
-
-    with open('data/splits.json', 'r') as f:
-        splits = json.load(f)
-
-    with open('data/entries.json', 'r') as f:
-        entries = json.load(f)
-
-    if split == 'train':
+    elif split == 'train':
         return df_full.loc[[entries[str(idx)] for idx in splits['trn']]].replace(labels)
     elif split == 'eval':
         return df_full.loc[[entries[str(idx)] for idx in splits['evl']]].replace(labels)

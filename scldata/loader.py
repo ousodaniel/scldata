8d651a77a135ef85263da897fe47e5d4a430838f
ousodaniel <ousodaniel@gmail.com>
2025-06-17 18:41:47 +0100

    fix path and target index substition

--- scldata/loader.py
+++ scldata/loader.py
@@ -1,14 +1,17 @@
 import json
 import pandas as pd
 from typing import Tuple, Union
+from pathlib import Path
 
-with open('data/labels.json', 'r') as f:
+_DATA_DIR = Path(__file__).parent/"data"
+
+with open(f'{_DATA_DIR}/labels.json', 'r') as f:
     labels = json.load(f)
 
-with open('data/splits.json', 'r') as f:
+with open(f'{_DATA_DIR}/splits.json', 'r') as f:
     splits = json.load(f)
 
-with open('data/entries.json', 'r') as f:
+with open(f'{_DATA_DIR}/entries.json', 'r') as f:
     entries = json.load(f)
 
 def load(split: Union[str, int, None] = None) -> Union[pd.DataFrame, Tuple[pd.DataFrame]]:
@@ -50,7 +53,7 @@ def load(split: Union[str, int, None] = None) -> Union[pd.DataFrame, Tuple[pd.Da
 
     Examples
     --------
-    >>> import scldata as sd
+    >>> import scldata as sd # or from scldata.loader import load or from scldata import load
     >>> df_full = sd.load("full")
     >>> df_full = sd.load()
     >>> df_train = sd.load("train")
@@ -59,23 +62,23 @@ def load(split: Union[str, int, None] = None) -> Union[pd.DataFrame, Tuple[pd.Da
     >>> df_kfold0 = sd.load(0) # retuns a tuple of dataframes with training and testing sets at index 0 and 1, respectively
     >>> df_kfold0 = sd.load("0")
 
-    .. note:: The SCL2205 dataset was curated from `UniProtKB`_, the latest release as of 24/01/2023. The indices are persistent identifiers consistent with *UniProtKB entry* feature.
+    .. note:: The SCL2205 dataset was curated from `UniProtKB`_, the latest release as of 24/01/2023. The indices are persistent identifiers consistent with *UniProtKB entry* identifier.
 
     """
 
-    df_full = pd.read_csv('data/scl2205.csv', index_col='entry')
+    df_full = pd.read_csv(f'{_DATA_DIR}/scl2205.csv', index_col='entry')
 
     if split is None or split == 'full':
-        return df_full.replace(labels)
+        return df_full.replace(labels['index_to_label'])
     elif split == 'train':
-        return df_full.loc[[entries[str(idx)] for idx in splits['trn']]].replace(labels)
+        return df_full.loc[[entries[str(idx)] for idx in splits['trn']]].replace(labels['index_to_label'])
     elif split == 'eval':
-        return df_full.loc[[entries[str(idx)] for idx in splits['evl']]].replace(labels)
+        return df_full.loc[[entries[str(idx)] for idx in splits['evl']]].replace(labels['index_to_label'])
     elif split == 'heldout':
-        return df_full.loc[[entries[str(idx)] for idx in splits['tst']]].replace(labels)
+        return df_full.loc[[entries[str(idx)] for idx in splits['tst']]].replace(labels['index_to_label'])
     elif (isinstance(split, int) or int(split)) and int(split) in range(5):
         k = int(split)
-        return (df_full.loc[[entries[str(idx)] for idx in splits['cv'][f'f{k}']['trn']]].replace(labels),
-                df_full.loc[[entries[str(idx)] for idx in splits['cv'][f'f{k}']['tst']]].replace(labels))
+        return (df_full.loc[[entries[str(idx)] for idx in splits['cv'][f'f{k}']['trn']]].replace(labels['index_to_label']),
+                df_full.loc[[entries[str(idx)] for idx in splits['cv'][f'f{k}']['tst']]].replace(labels['index_to_label']))
     else:
         raise ValueError('split must be either None, "full", "train", "eval", "heldout" or an integer(-string) representing a k-fold split, eg. 0 0r "0"')

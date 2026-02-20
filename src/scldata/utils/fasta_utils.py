f6cc51195c01ab5e5bed9515feec4478a9ba448f
ousodaniel <ousodaniel@gmail.com>
2026-02-20 08:32:28 +0000

    update doc string formatting

--- src/scldata/utils/fasta_utils.py
+++ src/scldata/utils/fasta_utils.py
@@ -4,18 +4,27 @@ from typing import TextIO, Optional
 
 
 def seq_df_to_fasta_handle(df: pd.DataFrame, header_col: Optional[str]=None, description_col: Optional[str]=None, seq_col: str= 'seq') -> TextIO:
-    '''
+    """
     Convert a pandas sequence DataFrame to a FASTA-formatted StringIO handle.
 
-    Params:
-        df Sequence DataFrame.
-        identifier_col Name of column to use as the fasta sequence header if not index/None. By default, the index is the header. All spaces are replaced with underscores.
-        description_col Name of column to use for sequence description, if 'description_col' is not None. By default, no description is used.
-        seq_col Column for the sequence data.
+    Parameters
+    ----------
+    `df`: `pandas.DataFrame`, required
+    `identifier_col`: `str`, optional
+    `description_col`: `str`, optional
+    `seq_col Column`: `str`, required
 
-    Returns:
-        StringIO handle.
-    '''
+    :param df: `pandas.DataFrame`, Sequence DataFrame.
+    :param header_col: `str`, Name of column to use as the fasta sequence header if not index/None. By default, the index is the header. All spaces are replaced with underscores.
+    :param description_col: `str`, Name of column to use for sequence description, if 'description_col' is not None. By default, no description is used.
+    :param seq_col: `str`, Column for the sequence data.
+
+    Returns
+    -------
+    `StringIO`
+
+    :return: A `StringIO` handle for the corresponding FASTA-formatted file.
+    """
     fasta_io = StringIO()
 
     header_col = header_col.strip() if header_col else None

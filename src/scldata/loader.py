5f697027f228359c8bdc48d0a97c353d2af8ffe8
ousodaniel <ousodaniel@gmail.com>
2026-02-20 16:27:09 +0000

    add output formating, rename split, update doc string

--- src/scldata/loader.py
+++ src/scldata/loader.py
@@ -1,8 +1,10 @@
-import argparse
 import json
 import pandas as pd
-from typing import Tuple, Union
+from typing import Tuple, Union, TextIO, Optional
 from pathlib import Path
+from pandas import DataFrame
+
+from scldata.utils.fasta_utils import seq_df_to_fasta_handle
 
 _DATA_DIR = Path(__file__).parent/'data'
 
@@ -15,24 +17,26 @@ with open(f'{_DATA_DIR}/splits.json', 'r') as f:
 with open(f'{_DATA_DIR}/entries.json', 'r') as f:
     entries = json.load(f)
 
-def load(split: Union[str, int, None] = None) -> Union[pd.DataFrame, Tuple[pd.DataFrame]]:
+def load(split: Union[str, int, None] = None, fasta: Optional[bool]=False) -> Union[DataFrame, TextIO, Tuple[Union[DataFrame, TextIO], Union[DataFrame, TextIO]]]:
     """
     A function for loading the full or split SCL2205 dataset.
 
     Parameters
     ----------
     `split`: `str` | `int`, optional
+    `fasta`: `bool`, optional
 
     :param split: `str` or `int` or `None`. If `str`, it can be either of "full", "train", "valid", "heldout". Integers can be provided in string form.
+    :param fasta: `bool`, Whether to return a fasta file instead of a dataframe.
 
     Returns
     -------
-    `pandas.DataFrame`
+    `pandas.DataFrame` | `TextIO handle`
 
-    :return: A `Pandas DataFrame` with UniProtKB unique entries as indices. The first column is "seq" (protein sequence), and the second is "scl" (subcellular location).
+    :return: A `Pandas DataFrame` with UniProtKB unique entries as indices. The first column is "seq" (protein sequence), and the second is "scl" (subcellular location). A `TextIO handle` for the fasta file equivalent, if `fasta` is `True`. A`tuple` of either types is returned if loading the cross-validation data splits.
 
     Descriptions
-    -----
+    -----`
     full : str
         The complete, unsplit SCL2205 dataset.
     train : str
@@ -48,18 +52,23 @@ def load(split: Union[str, int, None] = None) -> Union[pd.DataFrame, Tuple[pd.Da
 
     df_full = pd.read_csv(f'{_DATA_DIR}/scl2205.csv', index_col='entry')
 
+    def format_output(df: pd.DataFrame) -> Union[pd.DataFrame,TextIO]:
+        label_map = {int(k): v for k, v in labels['index_to_label'].items()}
+        df = df.replace(label_map)
+        return seq_df_to_fasta_handle(df, description_col='scl', seq_col='seq') if fasta else df
+
     if split is None or split == 'full':
-        return df_full.replace({int(k): v for k, v in labels['index_to_label'].items()})
+        return format_output(df_full)
     elif split == 'train':
-        return df_full.loc[[entries[str(idx)] for idx in splits['trn']]].replace({int(k): v for k, v in labels['index_to_label'].items()})
+        return format_output(df_full.loc[[entries[str(idx)] for idx in splits['trn']]])
     elif split == 'valid':
-        return df_full.loc[[entries[str(idx)] for idx in splits['vld']]].replace({int(k): v for k, v in labels['index_to_label'].items()})
+        return format_output(df_full.loc[[entries[str(idx)] for idx in splits['vld']]])
     elif split == 'heldout':
-        return df_full.loc[[entries[str(idx)] for idx in splits['tst']]].replace({int(k): v for k, v in labels['index_to_label'].items()})
+        return format_output(df_full.loc[[entries[str(idx)] for idx in splits['tst']]])
     elif (isinstance(split, int) or isinstance(int(split), int)) and int(split) in range(5):
         k = int(split)
-        return (df_full.loc[[entries[str(idx)] for idx in splits['cv'][f'f{k}']['trn']]].replace({int(k): v for k, v in labels['index_to_label'].items()}),
-                df_full.loc[[entries[str(idx)] for idx in splits['cv'][f'f{k}']['tst']]].replace({int(k): v for k, v in labels['index_to_label'].items()}))
+        return (format_output(df_full.loc[[entries[str(idx)] for idx in splits['cv'][f'f{k}']['trn']]]),
+                format_output(df_full.loc[[entries[str(idx)] for idx in splits['cv'][f'f{k}']['vld']]]))
     else:
         raise ValueError('split must be either None, "full", "train", "valid", "heldout" or an integer(-string) representing a k-fold split, eg. 0 0r "0"')
 

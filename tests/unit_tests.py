ea0883df67ad3eb6c8c21827c47d4ef019469ac3
ousodaniel <ousodaniel@gmail.com>
2026-02-22 07:46:25 +0000

    add fasta feature test units, update tests

--- tests/unit_tests.py
+++ tests/unit_tests.py
@@ -4,6 +4,7 @@ import sys
 from contextlib import redirect_stdout
 import unittest
 from io import StringIO
+from itertools import chain
 from pathlib import Path
 from unittest.mock import patch
 
@@ -46,6 +47,8 @@ class TestScldataCli(unittest.TestCase):
 
     def setUp(self):
         self.project_root = Path(__file__).resolve().parents[1]
+        self.script_path = self.project_root / 'src' / 'scldata' / '__init__.py'
+        self.output_file = 'test-scldata-file-saving'
 
     @patch('sys.argv', ['myscript.py', '--scls'])
     def test_main_with_scls(self):
@@ -76,33 +79,171 @@ class TestScldataCli(unittest.TestCase):
         self.assertEqual(expected_output.strip(), output.strip(), f'Expected 13 Classes:\n{expected_output}')
 
     def test_cli_execution(self):
-        script_path = self.project_root / 'src' / 'scldata' / '__init__.py'
         result = subprocess.run(
-            [sys.executable, script_path],
+            [sys.executable, self.script_path],
             stdout=subprocess.PIPE,
             stderr=subprocess.PIPE,
             text=True
         )
         self.assertEqual(result.returncode, 0, 'The script returned exit code 1, all was not ok')
-        self.assertIn('SCL2205 Full (Head):', result.stdout, 'The SCL2205 Full head output should be printed when "scldata" command (script) is run without any arguments')
+        self.assertIn('SCL2205 Full (Head):', result.stdout, 'The SCL2205 Full head output failed, should be printed when "scldata" command (script) is run without any arguments')
 
-    @patch('sys.argv', ['myscript.py', '--split', 'test'])
-    def test_invalid_option_arg(self):
+    @patch('sys.stderr', new_callable=StringIO)  # Silence error messages
+    @patch('sys.stdout', new_callable=StringIO)  # Silence standard prints
+    @patch('sys.argv', ['myscript.py', '--split', 'dev'])
+    def test_invalid_option_arg(self, mock_stdout, mock_stderr):
         with self.assertRaises(SystemExit):
             main()
 
-    def test_piping_without_BrokenPipeError(self):
-        cmd = f'{sys.executable} -m scldata --info all --split heldout | head'
+    def test_piping_broken_pipe_error(self):
+        cmd = [sys.executable, '-u', self.script_path, '--info', 'all', '--split', 'heldout']
+        process = subprocess.Popen(
+            cmd,
+            stdout=subprocess.PIPE,
+            stderr=subprocess.PIPE,
+            text=True
+        )
+
+        try:
+            if process.stdout:
+                process.stdout.read(10)
+                process.stdout.close()
+        finally:
+            _, stderr_data = process.communicate()
+
+        self.assertEqual(process.returncode, 0, f"Script failed with stderr: {stderr_data}")
+        self.assertNotIn('BrokenPipeError', stderr_data)
+        self.assertNotIn('Traceback', stderr_data)
+
+    def test_fasta_output(self):
+        cmd = [sys.executable, '-u', self.script_path, '-s', 'heldout', '-f', 'fasta']
+
         result = subprocess.run(
             cmd,
-            shell=True,
             capture_output=True,
             text=True
         )
-        self.assertEqual(result.returncode, 0, f'Script failed with stderr: {result.stderr}')
-        self.assertNotIn('BrokenPipeError', result.stderr, 'BrokenPipeError not handled')
-        self.assertNotIn('Traceback', result.stderr, 'Script crashed instead of exiting gracefully')
 
+        self.assertEqual(result.returncode, 0)
+
+        header_count = result.stdout.count('>')
+        self.assertGreater(header_count, 0, 'No FASTA headers found in output')
+
+    def test_fasta_tv_default_output_is_10_seq(self):
+        cmd = [sys.executable, '-u', self.script_path, '-s', 'heldout', '-f', 'fasta']
+
+        result = subprocess.run(
+            cmd,
+            capture_output=True,
+            text=True
+        )
+
+        self.assertEqual(result.returncode, 0)
+
+        header_count = result.stdout.count('>')
+        self.assertEqual(header_count, 10, 'Output is not 10 sequences')
+
+    def test_fasta_file_output_written_tv_fasta(self):
+        cmd = [sys.executable, '-u', self.script_path, '-s', 'heldout', '-f', 'fasta', '-o', self.output_file]
+
+        result = subprocess.run(
+            cmd,
+            capture_output=True,
+            text=True
+        )
+
+        self.assertEqual(result.returncode, 0)
+
+        self.assertTrue(Path(f'{self.output_file}-heldout-head.fasta').exists(), f'File "{self.output_file}-heldout-head.fasta" was not created.')
+        self.assertGreater(Path(f'{self.output_file}-heldout-head.fasta').stat().st_size, 0, f'File "{self.output_file}-heldout-head.fasta" was created but is empty.')
+
+    def test_fasta_file_output_written_cv_fasta(self):
+        cmd = [sys.executable, '-u', self.script_path, '-s', '0', '-f', 'fasta', '-o', self.output_file]
+
+        result = subprocess.run(
+            cmd,
+            capture_output=True,
+            text=True
+        )
+
+        self.assertEqual(result.returncode, 0)
+
+        self.assertTrue(Path(f'{self.output_file}-cv-0-train-head.fasta').exists(), f'File "{self.output_file}-cv-0-train-head.fasta" was not created.')
+        self.assertTrue(Path(f'{self.output_file}-cv-0-valid-head.fasta').exists(),
+                        f'File "{self.output_file}-cv-0-valid-head.fasta" was not created.')
+        self.assertGreater(Path(f'{self.output_file}-cv-0-train-head.fasta').stat().st_size, 0, f'File "{self.output_file}-cv-0-train-head.fasta" was created but is empty.')
+        self.assertGreater(
+            Path(f'{self.output_file}-cv-0-valid-head.fasta').stat().st_size, 0, f'File "{self.output_file}-cv-0-valid-head.fasta" was created but is empty.')
+
+    def test_fasta_file_output_written_tv_tsv(self):
+        cmd = [sys.executable, '-u', self.script_path, '-s', 'heldout', '-f', 'tsv', '-o', self.output_file]
+
+        result = subprocess.run(
+            cmd,
+            capture_output=True,
+            text=True
+        )
+
+        self.assertEqual(result.returncode, 0)
+
+        self.assertTrue(Path(f'{self.output_file}-heldout-head.tsv').exists(), f'File "{self.output_file}-heldout-head.tsv" was not created.')
+        self.assertGreater(Path(f'{self.output_file}-heldout-head.tsv').stat().st_size, 0, f'File "{self.output_file}-heldout-head.tsv" was created but is empty.')
+
+    def test_fasta_file_output_written_cv_tsv(self):
+        cmd = [sys.executable, '-u', self.script_path, '-s', '0', '-f', 'tsv', '-o', self.output_file]
+
+        result = subprocess.run(
+            cmd,
+            capture_output=True,
+            text=True
+        )
+
+        self.assertEqual(result.returncode, 0)
+
+        self.assertTrue(Path(f'{self.output_file}-cv-0-train-head.tsv').exists(), f'File "{self.output_file}-cv-0-train-head.tsv" was not created.')
+        self.assertTrue(Path(f'{self.output_file}-cv-0-valid-head.tsv').exists(),
+                        f'File "{self.output_file}-cv-0-valid-head.tsv" was not created.')
+        self.assertGreater(Path(f'{self.output_file}-cv-0-train-head.tsv').stat().st_size, 0, f'File "{self.output_file}-cv-0-train-head.tsv" was created but is empty.')
+        self.assertGreater(
+            Path(f'{self.output_file}-cv-0-valid-head.tsv').stat().st_size, 0, f'File "{self.output_file}-cv-0-valid-head.tsv" was created but is empty.')
+
+    def test_fasta_file_output_written_tv_csv(self):
+        cmd = [sys.executable, '-u', self.script_path, '-s', 'heldout', '-f', 'csv', '-o', self.output_file]
+
+        result = subprocess.run(
+            cmd,
+            capture_output=True,
+            text=True
+        )
+
+        self.assertEqual(result.returncode, 0)
+
+        self.assertTrue(Path(f'{self.output_file}-heldout-head.csv').exists(), f'File "{self.output_file}-heldout-head.csv" was not created.')
+        self.assertGreater(Path(f'{self.output_file}-heldout-head.csv').stat().st_size, 0, f'File "{self.output_file}-heldout-head.csv" was created but is empty.')
+
+    def test_fasta_file_output_written_cv_csv(self):
+        cmd = [sys.executable, '-u', self.script_path, '-s', '0', '-f', 'csv', '-o', self.output_file]
+
+        result = subprocess.run(
+            cmd,
+            capture_output=True,
+            text=True
+        )
+
+        self.assertEqual(result.returncode, 0)
+
+        self.assertTrue(Path(f'{self.output_file}-cv-0-train-head.csv').exists(), f'File "{self.output_file}-cv-0-train-head.csv" was not created.')
+        self.assertTrue(Path(f'{self.output_file}-cv-0-valid-head.csv').exists(),
+                        f'File "{self.output_file}-cv-0-valid-head.csv" was not created.')
+        self.assertGreater(Path(f'{self.output_file}-cv-0-train-head.csv').stat().st_size, 0, f'File "{self.output_file}-cv-0-train-head.csv" was created but is empty.')
+        self.assertGreater(
+            Path(f'{self.output_file}-cv-0-valid-head.csv').stat().st_size, 0, f'File "{self.output_file}-cv-0-valid-head.csv" was created but is empty.')
+
+    def tearDown(self):
+        directory = Path('.')
+        patterns = [f'{self.output_file}-*', 'test-scldata-saving*']
+        for file in chain(*(directory.glob(p) for p in patterns)):
+            file.unlink(missing_ok=True)
 
 if __name__ == '__main__':
     unittest.main()

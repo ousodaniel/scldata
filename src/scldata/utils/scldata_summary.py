e746482788ea699d84f8bdc4f9ff520e6c5a58e7
ousodaniel <ousodaniel@gmail.com>
2026-02-22 11:46:39 +0000

    add SCL2205 structure

---
+++ src/scldata/utils/scldata_summary.py
@@ -0,0 +1,22 @@
+splits_str = """\
+splits
+├── cv
+│   ├── f0
+│   │   ├── trn = [<int index>, ...] # counts: 15187
+│   │   └── vld = [<int index>, ...] # counts: 1256
+│   ├── f1
+│   │   ├── trn = [<int index>, ...] # counts: 15203
+│   │   └── vld = [<int index>, ...] # counts: 1240
+│   ├── f2
+│   │   ├── trn = [<int index>, ...] # counts: 15185
+│   │   └── vld = [<int index>, ...] # counts: 1258
+│   ├── f3
+│   │   ├── trn = [<int index>, ...] # counts: 15210
+│   │   └── vld = [<int index>, ...] # counts: 1233
+│   └── f4
+│       ├── trn = [<int index>, ...] # counts: 15265
+│       └── vld = [<int index>, ...] # counts: 1178
+├── vld = [<int index>, ...] # counts: 15183
+├── trn = [<int index>, ...] # counts: 1260
+└── tst = [<int index>, ...] # counts: 2631
+"""
